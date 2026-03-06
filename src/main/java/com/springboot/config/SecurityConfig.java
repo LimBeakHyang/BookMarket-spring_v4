94022ac025ed97dbed78cf74baa7ec22f09c18d1
BHyangie <bhyangie@gmail.com>
2026-03-06 18:09:58 +0900

    10단계: Spring Security 커스텀 로그인 페이지 및 로그인/로그아웃 기능 구현

---
+++ src/main/java/com/springboot/config/SecurityConfig.java
@@ -0,0 +1,70 @@
+package com.springboot.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	protected PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	protected UserDetailsService users() {
+		UserDetails admin = User.builder()
+				.username("Admin")
+				.password(passwordEncoder().encode("Admin1234"))
+				.roles("ADMIN")
+				.build();
+		return new InMemoryUserDetailsManager(admin);
+	}
+	
+	@Bean
+	protected SecurityFilterChain examMethod04(HttpSecurity http) throws Exception{
+		http
+		.csrf(AbstractHttpConfigurer::disable)
+		.authorizeHttpRequests(
+				authorizeRequests -> authorizeRequests
+				.requestMatchers("/books/add").hasRole("ADMIN")
+				.anyRequest().permitAll()	
+				)
+		/* Spring Security 기본 로그인 페이지 사용할때
+		 *.formLogin(Customizer.withDefaults()); */
+		
+		// 개발자가 기본 로그인 페이지 만들어서 사용할때
+		.formLogin(
+				formLogin -> formLogin
+				.loginPage("/login") // 로그인 페이지의 경로
+				.loginProcessingUrl("/login") // 로그인 페이지의 경로
+				.defaultSuccessUrl("/books/add") // 인증에 성공할 때의 경로
+				.failureUrl("/loginfailed") // 인정에 실패할 때의 경로
+				.usernameParameter("username") // 사용자 이름(계정)
+				.passwordParameter("password") //사용자 계정 비밀번호
+				.permitAll()	
+				)
+		// 로그아웃 기능 구현
+		.logout(
+				logout -> logout
+				.logoutUrl("/logout") // /logout으로 로그아웃 페이지 호출
+				.logoutSuccessUrl("/login") // 로그아웃에 성공하면 /login으로 자동 이동
+		);
+		
+		return http.build();
+		}
+	}
--- src/main/java/com/springboot/controller/BookController.java
+++ src/main/java/com/springboot/controller/BookController.java
@@ -44,139 +44,127 @@ public class BookController {
 	private BookService bookService;
 
 	@Value("${file.uploadDir}")
-	 String fileDir;
-	
+	String fileDir;
+
 	// @Autowired
 	// private UnitsInStockValidator unitsInStockValidator;
-	
+
 	@Autowired
 	private BookValidator bookvalidator;
-	
-	
+
 	@GetMapping
 	public String requestBookList(Model model) {
 		List<Book> list = bookService.getAllBookList();
-	    model.addAttribute("bookList", list); 
-		model.addAttribute("book", new Book()); 
-		return "books";
-	}	
-	/*
-	@GetMapping("/all")   
-	public String requestAllBooks(Model model) {
-		List<Book> list = bookService.getAllBookList(); 
-		model.addAttribute("bookList", list); 
+		model.addAttribute("bookList", list);
+		model.addAttribute("book", new Book());
 		return "books";
 	}
-	*/
-	
-	@GetMapping("/all")   
+	/*
+	 * @GetMapping("/all") public String requestAllBooks(Model model) { List<Book>
+	 * list = bookService.getAllBookList(); model.addAttribute("bookList", list);
+	 * return "books"; }
+	 */
+
+	@GetMapping("/all")
 	public ModelAndView requestAllBooks() {
-		ModelAndView modelAndView = new ModelAndView(); 
+		ModelAndView modelAndView = new ModelAndView();
 		List<Book> list = bookService.getAllBookList();
-		modelAndView.addObject("bookList", list); 
-		modelAndView.setViewName("books"); 
+		modelAndView.addObject("bookList", list);
+		modelAndView.setViewName("books");
 		return modelAndView;
 	}
-	
-	@GetMapping("/{category}") 
-	public String requestBooksByCategory(
-	           @PathVariable("category") String bookCategory, Model model) {  
-	    List<Book> booksByCategory =bookService.getBookListByCategory(bookCategory); 
-	    model.addAttribute("bookList", booksByCategory); 
-	    return "books"; 
-	 }
-	
+
+	@GetMapping("/{category}")
+	public String requestBooksByCategory(@PathVariable("category") String bookCategory, Model model) {
+		List<Book> booksByCategory = bookService.getBookListByCategory(bookCategory);
+		model.addAttribute("bookList", booksByCategory);
+		return "books";
+	}
+
 	@GetMapping("/filter/{bookFilter}")
-	public String requestBooksByFilter(
-	    @MatrixVariable(pathVar="bookFilter") Map<String,List<String>> bookFilter, Model model) {
-	    Set<Book> booksByFiter = bookService.getBookListByFilter(bookFilter);
-	    model.addAttribute("bookList", booksByFiter);
-	    return "books";
+	public String requestBooksByFilter(@MatrixVariable(pathVar = "bookFilter") Map<String, List<String>> bookFilter,
+			Model model) {
+		Set<Book> booksByFiter = bookService.getBookListByFilter(bookFilter);
+		model.addAttribute("bookList", booksByFiter);
+		return "books";
 	}
-	
-	@GetMapping("/book") 
-	public String requestBookById(@RequestParam("id") String bookId, Model model) {  
-	   Book bookById = bookService.getBookById(bookId);
-	   model.addAttribute("book", bookById );
-	   return "book";
-	}
-	
-	 @GetMapping("/add")
-	 public String requestAddBookForm(Model model) {
-		 model.addAttribute("book", new Book());
-		 return "addBook";
-	 }
-	 
-	 
-	 @PostMapping("/add")	  
-	 public String submitAddNewBook(@Valid @ModelAttribute Book book, BindingResult bindingResult) {
-		 
-		 if(bindingResult.hasErrors()) {
-			 return "addBook";
-		 }
-		 
-	 MultipartFile bookImage = book.getBookImage();  
 
-		String saveName = bookImage.getOriginalFilename();   
-		File saveFile = new File(fileDir, saveName); 
-			
-			if (bookImage != null && !bookImage.isEmpty()) {
-				try {
-					bookImage.transferTo(saveFile);  
-				} catch (Exception e) {
-					throw new RuntimeException("도서 이미지 업로드가 실패하였습니다", e);
-				}
+	@GetMapping("/book")
+	public String requestBookById(@RequestParam("id") String bookId, Model model) {
+		Book bookById = bookService.getBookById(bookId);
+		model.addAttribute("book", bookById);
+		return "book";
+	}
+
+	@GetMapping("/add")
+	public String requestAddBookForm(Model model) {
+		model.addAttribute("book", new Book());
+		return "addBook";
+	}
+
+	@PostMapping("/add")
+	public String submitAddNewBook(@Valid @ModelAttribute Book book, BindingResult bindingResult) {
+
+		if (bindingResult.hasErrors()) {
+			return "addBook";
+		}
+
+		MultipartFile bookImage = book.getBookImage();
+
+		String saveName = bookImage.getOriginalFilename();
+		File saveFile = new File(fileDir, saveName);
+
+		if (bookImage != null && !bookImage.isEmpty()) {
+			try {
+				bookImage.transferTo(saveFile);
+			} catch (Exception e) {
+				throw new RuntimeException("도서 이미지 업로드가 실패하였습니다", e);
 			}
-		  book.setFileName(saveName);	
-	      bookService.setNewBook(book); 
-		 
-		 
-	      
-	  
-	      return "redirect:/books";
-	 }
-	
-	
-	 @GetMapping("/download")
-	 public void downloadBookImage(@RequestParam("file") String paramKey,                         
-		                         HttpServletResponse response) throws IOException {
+		}
+		book.setFileName(saveName);
+		bookService.setNewBook(book);
 
-		
-		 if (paramKey == null) {
-	            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-	            return;
-	     }
-		 
-	    File imageFile = new File(fileDir + paramKey );	   
-	    
-	    if (imageFile.exists() == false) {
-            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-            return;
-        }
-	    response.setStatus(HttpServletResponse.SC_OK);    
-	    response.setContentType("application/download");
-	    response.setContentLength((int)imageFile.length());
-	    response.setHeader("Content-disposition", "attachment;filename=\"" + paramKey + "\"");	   
-	    OutputStream os = response.getOutputStream();
-	    FileInputStream fis = new FileInputStream(imageFile);
-	    FileCopyUtils.copy(fis, os);
-	    fis.close();
-	    os.close();
-	        
-    }
-	 
-	 @ModelAttribute 
-	 public void addAttributes(Model model) { 
-	     model.addAttribute("addTitle", "신규 도서 등록");
-	 }
-	 
-	 @InitBinder
-	 public void initBinder(WebDataBinder binder) {
+		return "redirect:/books";
+	}
+
+	@GetMapping("/download")
+	public void downloadBookImage(@RequestParam("file") String paramKey, HttpServletResponse response)
+			throws IOException {
+
+		if (paramKey == null) {
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			return;
+		}
+
+		File imageFile = new File(fileDir + paramKey);
+
+		if (imageFile.exists() == false) {
+			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+			return;
+		}
+		response.setStatus(HttpServletResponse.SC_OK);
+		response.setContentType("application/download");
+		response.setContentLength((int) imageFile.length());
+		response.setHeader("Content-disposition", "attachment;filename=\"" + paramKey + "\"");
+		OutputStream os = response.getOutputStream();
+		FileInputStream fis = new FileInputStream(imageFile);
+		FileCopyUtils.copy(fis, os);
+		fis.close();
+		os.close();
+
+	}
+
+	@ModelAttribute
+	public void addAttributes(Model model) {
+		model.addAttribute("addTitle", "신규 도서 등록");
+	}
+
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
 		// binder.setValidator(unitsInStockValidator);
-		 binder.setValidator(bookvalidator);
-		 binder.setAllowedFields("bookId","name","unitPrice","author","description","publisher","category",
-	                              "unitsInStock","totalPages", "releaseDate", "condition", "bookImage");
-	 }
+		binder.setValidator(bookvalidator);
+		binder.setAllowedFields("bookId", "name", "unitPrice", "author", "description", "publisher", "category",
+				"unitsInStock", "totalPages", "releaseDate", "condition", "bookImage");
+	}
 
-	
 }
